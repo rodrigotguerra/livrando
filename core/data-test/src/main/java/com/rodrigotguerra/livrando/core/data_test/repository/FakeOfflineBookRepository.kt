2ab5db7119b9e5e0849a4bf4095a6bc2c716270c
Rodrigo T. Guerra <rodrigotguerra2@gmail.com>
2024-09-10 18:30:50 -0300

    refactor: Fake Repository correction

--- core/data-test/src/main/java/com/rodrigotguerra/livrando/core/data_test/repository/FakeOfflineBookRepository.kt
+++ core/data-test/src/main/java/com/rodrigotguerra/livrando/core/data_test/repository/FakeOfflineBookRepository.kt
@@ -3,16 +3,19 @@ package com.rodrigotguerra.livrando.core.data_test.repository
 import com.rodrigotguerra.livrando.core.data.repository.BookRepository
 import com.rodrigotguerra.livrando.core.model.Book
 
+
 class FakeOfflineBookRepository : BookRepository {
 
     private val bookItems = mutableListOf<Book>()
 
-    override suspend fun insertBook(book: Book) {
+    override suspend fun insertBook(book: Book): Result<Unit> {
         bookItems.add(book)
+        return Result.success(Unit)
     }
 
-    override suspend fun deleteBook(book: Book) {
+    override suspend fun deleteBook(book: Book): Result<Unit>{
         bookItems.remove(book)
+        return Result.success(Unit)
     }
 
     override suspend fun getBooks(): List<Book> {
